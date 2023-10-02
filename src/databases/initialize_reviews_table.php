ae61ccaafdda8a7ed0978ae0392f13ed06877220
suuu0122 <suuu0122lab@gmail.com>
2023-10-02 20:44:13 +0900

    separate sensitive information from code

--- src/databases/initialize_reviews_table.php
+++ src/databases/initialize_reviews_table.php
@@ -1,8 +1,18 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
 function dbConnect()
 {
-    $link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+    $dotenv->safeLoad();
+    
+    $dbHost = $_ENV['DB_HOST'];
+    $dbUsername = $_ENV['DB_USERNAME'];
+    $dbPassword = $_ENV['DB_PASSWORD'];
+    $dbName = $_ENV['DB_NAME'];
+
+    $link = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
     if (!$link) {
         echo 'Error: データベースに接続できません' . PHP_EOL;
         echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
