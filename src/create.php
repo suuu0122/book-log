fd1459ec299dfae8677a8831582da7a86e17432d
suuu0122 <suuu0122lab@gmail.com>
2023-10-03 22:37:59 +0900

    added validate function

--- src/create.php
+++ src/create.php
@@ -27,6 +27,40 @@ function createReview($link, $review)
     }
 }
 
+function validate($review)
+{
+    $errors = [];
+
+    if (!strlen($review['title'])) {
+        $errors['title'] = '書籍名を入力してください';
+    } elseif (strlen($review['title']) > 255) {
+        $errors['title'] = '書籍名は255文字以内で入力してください';
+    }
+
+    if (!strlen($review['author'])) {
+        $errors['author'] = '著者名を入力してください';
+    } elseif (strlen($review['author']) > 100) {
+        $errors['author'] = '著者名は100文字以内で入力してください';
+    }
+
+    if (!strlen($review['status'])) {
+        $errors['status'] = '読書状況を選択してください';
+    }
+
+    $score = (int) $review['score'];
+    if ($score < 1 || 5 < $score) {
+        $errors['score'] = '評価は1以上5以下の整数で入力してください';
+    }
+
+    if (!strlen($review['summary'])) {
+        $errors['summary'] = '感想を入力してください';
+    } elseif (strlen($review['summary']) > 1000) {
+        $errors['summary'] = '感想は1000文字以内で入力してください';
+    }
+
+    return $errors;
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $review = [
         'title' => $_POST['title'],
@@ -36,13 +70,70 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'summary' => $_POST['summary']
     ];
 
-    // ToDo:バリデーション
-
-    $link = dbConnect();
-
-    createReview($link, $review);
-
-    mysqli_close($link);
+    $errors = validate($review);
+    if (!count($errors)) {
+        $link = dbConnect();
+        createReview($link, $review);
+        mysqli_close($link);
+        header("Location: index.php");
+    }
 }
 
-header("Location: index.php");
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>読書ログ登録</title>
+</head>
+
+<body>
+    <h1>読書ログの登録</h1>
+    <form action="create.php" method="post">
+        <?php if (count($errors)) : ?>
+            <ul>
+                <?php foreach ($errors as $error) : ?>
+                    <li><?php echo $error; ?></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+        <div>
+            <label for="title">書籍名</label>
+            <input type="text" name="title" id="title">
+        </div>
+        <div>
+            <label for="author">著者名</label>
+            <input type="text" name="author" id="author">
+        </div>
+        <div>
+            <label for="status">読書状況</label>
+            <div>
+                <div>
+                    <input type="radio" name="status" id="unread" value="未読">
+                    <label for="unread">未読</label>
+                </div>
+                <div>
+                    <input type="radio" name="status" id="current" value="読書中">
+                    <label for="current">読書中</label>
+                </div>
+                <div>
+                    <input type="radio" name="status" id="complete" value="読了">
+                    <label for="complete">読了</label>
+                </div>
+            </div>
+        </div>
+        <div>
+            <label for="score">評価（5点満点の整数）</label>
+            <input type="number" name="score" id="score">
+        </div>
+        <div>
+            <label for="summary">感想</label>
+            <textarea type="text" name="summary" id="summary" rows="10"></textarea>
+        </div>
+        <button type="submit">登録する</button>
+    </form>
+</body>
+
+</html>
