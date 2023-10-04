15d73f016ffe5904728cfc06867c0db9e13171f3
suuu0122 <suuu0122lab@gmail.com>
2023-10-04 23:09:25 +0900

    separated php and html

---
+++ src/views/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../stylesheets/css/app.css">
+    <title>読書ログの一覧</title>
+</head>
+
+<body>
+    <header class="navbar shadow-sm p-3 mb-5 bg-white">
+        <h1 class="h2">
+            <a class="text-body text-decoration-none" href="index.php">読書ログ</a>
+        </h1>
+    </header>
+    <div class="container">
+        <h1 class="h2 text-dark mt-4 mb-4">読書ログの一覧</h1>
+        <a href="new.php">読書ログを登録する</a>
+        <main>
+            <section>
+                <h3>書籍名</h3>
+                <div>著者名</div>
+                <div>読書状況</div>
+                <div>評価</div>
+                <div>感想</div>
+            </section>
+        </main>
+    </div>
+</body>
+
+</html>
