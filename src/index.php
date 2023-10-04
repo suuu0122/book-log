abdf4d2af340d8b97def1af29839e889d41714ae
suuu0122 <suuu0122lab@gmail.com>
2023-10-05 00:03:00 +0900

    added list view function

--- src/index.php
+++ src/index.php
@@ -1,5 +1,28 @@
 <?php
 
+require_once __DIR__ . '/lib/mysqli.php';
+
+function listReviews($link)
+{
+    $reviews = [];
+
+    $sql = <<<EOT
+    SELECT title, author, status, score, summary FROM reviews
+    EOT;
+    $results = mysqli_query($link, $sql);
+
+    while ($review = mysqli_fetch_assoc($results)) {
+        $reviews[] = $review;
+    }
+
+    mysqli_free_result($results);
+
+    return $reviews;
+}
+
+$link = dbConnect();
+$reviews = listReviews($link);
+
 $title = '読書ログの一覧';
 $content = __DIR__ . '/views/index.php';
 include __DIR__ . '/views/layout.php';
--- src/views/index.php
+++ src/views/index.php
@@ -1,10 +1,14 @@
-<a href="new.php">読書ログを登録する</a>
+<a class="btn btn-primary" href="new.php">読書ログを登録する</a>
 <main>
-    <section>
-        <h3>書籍名</h3>
-        <div>著者名</div>
-        <div>読書状況</div>
-        <div>評価</div>
-        <div>感想</div>
-    </section>
+    <?php if (count($reviews) > 0) : ?>
+        <?php foreach ($reviews as $review) : ?>
+            <section class="p-4">
+                <h3><?php echo $review['title']; ?></h3>
+                <div>著者名: <?php echo $review['author']; ?> 読書状況: <?php echo $review['status']; ?>  評価: <?php echo $review['score']; ?></div>
+                <div><?php echo $review['summary']; ?></div>
+            </section>
+        <?php endforeach; ?>
+    <?php else : ?>
+        <p class="p-4">読書ログが登録されていません</p>
+    <?php endif ?>
 </main>
