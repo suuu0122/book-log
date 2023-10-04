caf0942d612016c05271af5b953b1f0b51b5296a
suuu0122 <suuu0122lab@gmail.com>
2023-10-04 23:31:59 +0900

    standardized layout

--- src/index.php
+++ src/index.php
@@ -1,3 +1,5 @@
 <?php
 
-include 'views/index.php';
+$title = '読書ログの一覧';
+$content = __DIR__ . '/views/index.php';
+include __DIR__ . '/views/layout.php';
--- src/new.php
+++ src/new.php
@@ -10,4 +10,6 @@ $review = [
 
 $errors = [];
 
-include 'views/new.php';
+$title = '読書ログの登録';
+$content = __DIR__ . '/views/new.php';
+include __DIR__ . '/views/layout.php';
