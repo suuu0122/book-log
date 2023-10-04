e1aa38a28bc769f58238de91c5d6befdd9198318
suuu0122 <suuu0122lab@gmail.com>
2023-10-05 02:46:16 +0900

    fixed bootstrap

--- src/views/index.php
+++ src/views/index.php
@@ -1,12 +1,14 @@
-<a class="btn btn-primary" href="new.php">読書ログを登録する</a>
+<a class="btn btn-primary mb-4" href="new.php">読書ログを登録する</a>
 <main>
     <?php if (count($reviews) > 0) : ?>
         <?php foreach ($reviews as $review) : ?>
-            <section class="p-4">
-                <h3><?php echo escape($review['title']); ?></h3>
-                <div>著者名: <?php echo escape($review['author']); ?> 読書状況: <?php echo escape($review['status']); ?>  評価: <?php echo escape($review['score']); ?></div>
-                <div><?php echo escape($review['summary']); ?></div>
-            </section>
+            <div class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <h3 class="card-title"><?php echo escape($review['title']); ?></h3>
+                    <p class="cart-text">著者名: <?php echo escape($review['author']); ?> 読書状況: <?php echo escape($review['status']); ?>  評価: <?php echo escape($review['score']); ?></p>
+                    <div><?php echo escape($review['summary']); ?></div>
+                </div>
+            </div>
         <?php endforeach; ?>
     <?php else : ?>
         <p class="p-4">読書ログが登録されていません</p>
