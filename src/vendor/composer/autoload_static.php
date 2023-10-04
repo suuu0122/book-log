5da0b2e77491fa01faeadc17ffccf6352042928d
suuu0122 <suuu0122lab@gmail.com>
2023-10-04 13:17:01 +0900

    installed scssphp

--- src/vendor/composer/autoload_static.php
+++ src/vendor/composer/autoload_static.php
@@ -18,6 +18,7 @@ class ComposerStaticInit2b363929071d5b101abe7ca5bc7cd764
             'Symfony\\Polyfill\\Php80\\' => 23,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
+            'ScssPhp\\ScssPhp\\' => 16,
         ),
         'P' => 
         array (
@@ -45,6 +46,10 @@ class ComposerStaticInit2b363929071d5b101abe7ca5bc7cd764
         'Symfony\\Polyfill\\Ctype\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
+        'ScssPhp\\ScssPhp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/scssphp/scssphp/src',
         ),
         'PhpOption\\' => 
         array (
